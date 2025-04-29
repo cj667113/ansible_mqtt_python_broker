ba04276a4c3cd97648a4245edef248f1e97c14c0
Chris Johnston <40300884+cj667113@users.noreply.github.com>
2025-04-29 12:35:42 -0700

    Update startup.sh amqtt

--- startup.sh
+++ startup.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-nohup hbmqtt -c mqtt.conf < /dev/null > /dev/null 2>&1 &
+nohup amqtt -c mqtt.conf < /dev/null > /dev/null 2>&1 &
 nohup python3 mrp/mrps.py -i "{{ inventory_hostname }}"  -p 1883 < /dev/null > /dev/null 2>&1>
-
